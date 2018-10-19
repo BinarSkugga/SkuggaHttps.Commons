509291548f6615f0e6cb89c0f3b02e11404579f6
Charles Smith <charles@sollum.tech>
2018-10-19 11:57:30 -0400

    Fixes for SkuggaHttps#16

--- src/main/java/com/binarskugga/skuggahttps/HttpJsonHandler.java
+++ src/main/java/com/binarskugga/skuggahttps/HttpJsonHandler.java
@@ -1,11 +1,15 @@
 package com.binarskugga.skuggahttps;
 
 import java.nio.charset.*;
+import java.util.Collection;
 
 public interface HttpJsonHandler extends HttpResponseHandler {
 
 	<T> String toJson(Class<T> clazz, T response);
 	<T> T fromJson(Class<T> clazz, String body);
+
+	<T> String toJson(Class<Collection> collectionClazz, Class<T> clazz, Collection<T> response);
+	<T> Collection<T> fromJson(Class<Collection> collectionClazz, Class<T> clazz, String body);
 
 	@Override
 	default String defaultContentType() {
@@ -20,6 +24,16 @@ public interface HttpJsonHandler extends HttpResponseHandler {
 	@Override
 	default <T> T fromBytes(Class<T> clazz, byte[] body) {
 		return this.fromJson(clazz, new String(body, Charset.forName("UTF-8")));
+	}
+
+	@Override
+	default <T> byte[] toBytes(Class<Collection> collectionClazz, Class<T> clazz, Collection<T> response) {
+		return this.toJson(collectionClazz, clazz, response).getBytes(Charset.forName("UTF-8"));
+	}
+
+	@Override
+	default <T> Collection<T> fromBytes(Class<Collection> collectionClazz, Class<T> clazz, byte[] body) {
+		return this.fromJson(collectionClazz, clazz, new String(body, Charset.forName("UTF-8")));
 	}
 
 }
--- src/main/java/com/binarskugga/skuggahttps/HttpResponseHandler.java
+++ src/main/java/com/binarskugga/skuggahttps/HttpResponseHandler.java
@@ -1,9 +1,14 @@
 package com.binarskugga.skuggahttps;
 
+import java.util.Collection;
+
 public interface HttpResponseHandler {
 
 	String defaultContentType();
 	<T> byte[] toBytes(Class<T> clazz, T response);
 	<T> T fromBytes(Class<T> clazz, byte[] body);
+
+	<T> byte[] toBytes(Class<Collection> collectionClazz, Class<T> clazz, Collection<T> response);
+	<T> Collection<T> fromBytes(Class<Collection> collectionClazz, Class<T> clazz, byte[] body);
 
 }
